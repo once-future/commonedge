dda8fce37fffed9f2da13bf29aeff938b8692520
Marty Spellerberg <marty@martyspellerberg.com>
2020-09-03 14:05:59 -0500

    WPE stuff

---
+++ .wpengine-conf/wp-config-site.php
@@ -0,0 +1,5 @@
+<?php
+# GENERATED AUTOMATICALLY ON 2020-09-03 13:50:34.851012 -0500 CDT m=+2767.622537701
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "commonedge.wpengine.com";
+$wpengine_primary_original_ip_address = "104.199.119.213";
